1234f2b2998e96f720ecbe3a0a312798e7c64847
Elioenai Roberto <42978220+LeoniSZ@users.noreply.github.com>
2020-12-07 23:48:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ const opcoes = {
     scizzors: 3
 };
 
-
-/*
-
-returnOption arrow function with variable
-
-
-const returnOption = number => {
-    if(number === 1){
-        return 'Rock'
-    }
-    if(number === 2){
-        return 'Paper'
-    }
-    if(number === 3){
-        return 'Scizzors'
-    }
-}
-*/
-
 function returnOption(number){
     if(number === 1){
         return 'Rock'
@@ -43,54 +24,56 @@ function returnOption(number){
     }
 }
 
+const err = valor => {
+    if(valor !== 1 && valor !== 2 && valor !== 3){
+        console.log('invalid input');
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const flag = true;
 
 
-rl.question('What you gonna do?', resposta =>{
-    
-    const escolhaDoUsuario  = opcoes[resposta];
+rl.question('What you gonna do? ', resposta =>{
+
+    const escolhaDoUsuario  = opcoes[resposta.toLowerCase()];
     const escolhaDoComputador = Math.round(Math.random() * 2) + 1;
     
-    console.log(`Your play: ${returnOption(escolhaDoUsuario)}\nMachine play: ${returnOption(escolhaDoComputador)}`)
-    //Rock win conditions
-    if(escolhaDoUsuario === 1 && escolhaDoComputador === 1){
-        console.log(`Oh my! That's a draw!`);
-    }else if(escolhaDoUsuario === 1 && escolhaDoComputador === 2){
-        console.log(`This is bad... you lost`);
-    }else if(escolhaDoUsuario === 1 && escolhaDoComputador === 3){
-        console.log(`Oh my! You won!`);
+    if(err(escolhaDoUsuario)){
+        rl.close();
     }
+    else{
+        console.log(`Your play: ${returnOption(escolhaDoUsuario)}\nMachine play: ${returnOption(escolhaDoComputador)}`)
+        //Rock win conditions
+        if(escolhaDoUsuario === 1 && escolhaDoComputador === 1){
+            console.log(`Oh my! That's a draw!`);
+        }else if(escolhaDoUsuario === 1 && escolhaDoComputador === 2){
+            console.log(`This is bad... you lost`);
+        }else if(escolhaDoUsuario === 1 && escolhaDoComputador === 3){
+            console.log(`Oh my! You won!`);
+        }
+        
+        //Paper win conditions
+        if(escolhaDoUsuario === 2 && escolhaDoComputador === 2){
+            console.log(`Oh my! That's a draw!`);
+        }else if(escolhaDoUsuario === 2 && escolhaDoComputador === 3){
+            console.log(`This is bad... you lost`);
+        }else if(escolhaDoUsuario === 2 && escolhaDoComputador === 1){
+            console.log(`Oh my! You won!`);
+        }
     
-    //Paper win conditions
-    if(escolhaDoUsuario === 2 && escolhaDoComputador === 2){
-        console.log(`Oh my! That's a draw!`);
-    }else if(escolhaDoUsuario === 2 && escolhaDoComputador === 3){
-        console.log(`This is bad... you lost`);
-    }else if(escolhaDoUsuario === 2 && escolhaDoComputador === 1){
-        console.log(`Oh my! You won!`);
+        //Scissor win conditions
+        if(escolhaDoUsuario === 3 && escolhaDoComputador === 3){
+            console.log(`Oh my! That's a draw!`);
+        }else if(escolhaDoUsuario === 3 && escolhaDoComputador === 1){
+           console.log(`This is bad... you lost`);
+        }else if(escolhaDoUsuario === 3 && escolhaDoComputador === 2){
+            console.log(`Oh my! You won!`);
+        }
+
+        rl.close();
     }
-
-
-    //Scissor win conditions
-    if(escolhaDoUsuario === 3 && escolhaDoComputador === 3){
-        console.log(`Oh my! That's a draw!`);
-    }else if(escolhaDoUsuario === 3 && escolhaDoComputador === 1){
-        console.log(`This is bad... you lost`);
-    }else if(escolhaDoUsuario === 3 && escolhaDoComputador === 2){
-        console.log(`Oh my! You won!`);
-    }
-
-
-    rl.close();
 })
-
-
-
-
-
-
-
-
-
-
-
-
